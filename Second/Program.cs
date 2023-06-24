e2e196030b794d77f86bea437fb1870d871f0da1
Sergei Ivanov <willanov@yandex.ru>
2023-06-24 09:41:13 +0500

    Added Second

---
+++ Second/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintSumm(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов = {summ} ");
+        return;
+    }
+    PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string mes)
+{
+    Console.Write(mes);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+
+Console.Clear();
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+PrintSumm(m, n, temp = 0);
+
