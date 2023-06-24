54f51800a5408f839353714468695eb31e55c029
Sergei Ivanov <willanov@yandex.ru>
2023-06-24 09:29:19 +0500

    Added First

---
+++ First/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+void PrintNumber(int n, int count)
+{
+    if (count > n) return;
+    PrintNumber(n, count + 1);
+    Console.Write(count + ", ");
+}
+
+int InputNumbers(string mes)
+{
+    Console.Write(mes);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+int n = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
